a8895f4d8256d373781cdc2c948baff71d81f0e5
Dave Richards <DaveRichards71@gmail.com>
2024-11-04 14:20:40 -0500

    Remove backend_django

--- backend_django/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('tasks/', views.TaskListCreate.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', views.TaskDelete.as_view(), name='delete-task'),    
-]
