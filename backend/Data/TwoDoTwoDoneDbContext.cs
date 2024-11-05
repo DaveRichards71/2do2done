1b36278951b093ca6021b19c3fd7262ebf873b16
Dave Richards <DaveRichards71@gmail.com>
2024-11-05 10:45:15 -0500

    * Added Data and Domain projects * Domain - model classes * Data - DbContext

---
+++ backend/Data/TwoDoTwoDoneDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class TwoDoTwoDoneDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Domain.Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // For Development Only!
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Initial Catalog=TwoDoTwoDone_EfCore; Encrypt=False");
+        }
+
+    }
+}
